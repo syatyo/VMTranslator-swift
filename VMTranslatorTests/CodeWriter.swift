0bd6b400d05869f234f52c4e3433797bf7df03c2
syatyo <yoshiharuseijin@gmail.com>
2019-02-24 00:05:54 +0900

    test: add test for setFileName.

--- VMTranslatorTests/CodeWriter.swift
+++ VMTranslatorTests/CodeWriter.swift
@@ -17,17 +17,12 @@ class CodeWriter: XCTestCase {
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    func testSetFileName() {
+        let outputDirPath = "/Users/user/Documents/GitHub/VMTranslator-swift/"
+        let codeWriter = CodeWriter(outputDirPath: outputDirPath)
+        codeWriter.setFileName("test.asm")
+        XCTAssertEqual(codeWriter.outputFilePath, "/Users/user/Documents/GitHub/VMTranslator-swift/test.asm")
     }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
+    
 }
