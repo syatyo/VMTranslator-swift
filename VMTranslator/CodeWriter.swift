7592af82421d31332d6d7b49d046aab4587bcd2c
syatyo <yoshiharuseijin@gmail.com>
2019-02-24 00:48:51 +0900

    feat: implement close.

--- VMTranslator/CodeWriter.swift
+++ VMTranslator/CodeWriter.swift
@@ -11,6 +11,7 @@ import Foundation
 struct CodeWriter {
     let outputDirPath: String
     private var fileName: String?
+    private var asmCommands: String = ""
     
     init(outputDirPath: String) {
         self.outputDirPath = outputDirPath
@@ -23,6 +24,11 @@ struct CodeWriter {
     var outputFilePath: String? {
         guard let fileName = self.fileName else { return nil }
         return outputDirPath + fileName
+    }
+    
+    func close() throws {
+        let url = URL(fileURLWithPath: outputFilePath ?? "")
+        try asmCommands.write(to: url, atomically: false, encoding: .utf8)
     }
     
 }
