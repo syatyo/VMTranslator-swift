3c3c26a890f328c383d55706c19c34154ca0136e
syatyo <yoshiharuseijin@gmail.com>
2019-03-25 01:14:50 +0900

    feat: implementation of IfGoto VMCommand.

---
+++ VMTranslator/VMCommand/IfGoto.swift
@@ -0,0 +1,26 @@
+//
+//  IfGoto.swift
+//  VMTranslator
+//
+//  Created by 山田良治 on 2019/03/25.
+//  Copyright © 2019 山田良治. All rights reserved.
+//
+
+import Foundation
+
+struct IfGoto {
+    let labelName: String
+    
+    func execute() -> String {
+        var builder = CommandBuilder()
+        // Start pop
+        builder.add(ATCommand(difinedSymbol: .sp))
+        builder.add(AssignCommand(destination: .am, computation: .mMinusOne))
+        builder.add(AssignCommand(destination: .d, computation: .m))
+        // End pop
+        builder.add(ATCommand(label: labelName))
+        builder.add(ConditionCommand(operand: .d, conditionType: .jne))
+        return builder.build()
+    }
+    
+}
